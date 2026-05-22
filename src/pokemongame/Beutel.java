ad426fe61bd37dfa132ae099cd98b78f71c88ef5
Tobias Walter <tob.dev@outlook.com>
2026-05-23 01:05:35 +0200

    docs: ergaenze Javadoc-Dokumentation in Beutel.java
    - Fuege Klassenbeschreibung zur Definition der Inventarverwaltung hinzu
    - Dokumentiere den Konstruktor fuer das initiale Item-Array
    - Beschreibe die Getter-Schnittstellen fuer Inventarabfragen mit @return

--- src/pokemongame/Beutel.java
+++ src/pokemongame/Beutel.java
@@ -2,27 +2,41 @@ package pokemongame;
 
 /**
  * Verwaltet exakt das Inventar des Spielers (Reines Daten-Objekt).
- * Weiß nichts von Konsolen-Ausgaben oder Scannern.
+ * Diese Klasse ist entkoppelt von Konsolen-Ausgaben oder Eingabe-Scannern.
  */
 public class Beutel {
 
     private Item[] inventar;
 
+    /**
+     * Erstellt ein neues Beutel-Objekt mit einem Startinventar.
+     * * @param startInventar Das Array der initialen Gegenstaende
+     */
     public Beutel(Item[] startInventar) {
         this.inventar = startInventar;
     }
 
-    // Gibt das gesamte Inventar-Array zurück, damit die UI es anzeigen kann
+    /**
+     * Gibt das gesamte Inventar-Array zurueck, damit die UI es anzeigen kann.
+     * * @return Das Array der im Beutel enthaltenen Items
+     */
     public Item[] getInventar() {
         return this.inventar;
     }
 
-    // Liefert die Anzahl der verschiedenen Item-Typen im Beutel
+    /**
+     * Liefert die Anzahl der verschiedenen Item-Typen im Beutel.
+     * * @return Die Laenge des Inventar-Arrays
+     */
     public int getAnzahlItemTypen() {
         return inventar.length;
     }
 
-    // Holt ein spezifisches Item über seinen Index
+    /**
+     * Holt ein spezifisches Item ueber seinen Array-Index.
+     * * @param index Der Verzeichnis-Index des gesuchten Gegenstands
+     * @return Das Item-Objekt an der Stelle des Index, oder null bei ungueltigen Indizes
+     */
     public Item getItem(int index) {
         if (index >= 0 && index < inventar.length) {
             return inventar[index];
