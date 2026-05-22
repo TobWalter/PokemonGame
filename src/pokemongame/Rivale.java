728ccb68a573c1e0cf8e44c98792e171d16742ce
Tobias Walter <tob.dev@outlook.com>
2026-05-23 01:22:43 +0200

    docs: ergaenze Javadoc-Dokumentation in Rivale.java
    - Fuege formale Beschreibungen fuer die KI-Methode fuehreZufallsAktionAus hinzu
    - Dokumentiere die Parameter fuer Angreifer, Ziel und Zufallsgenerator via @param

--- src/pokemongame/Rivale.java
+++ src/pokemongame/Rivale.java
@@ -4,11 +4,16 @@ import java.util.Random;
 
 /**
  * Steuert die einfache KI des gegnerischen Rivalen.
+ * Waehlt automatisiert Aktionen aus, ohne dass eine Benutzereingabe erforderlich ist.
  */
 public class Rivale {
 
     /**
-     * Wählt zufällig eine der 4 Attacken des Rivalen-Pokémons und führt sie aus.
+     * Waehlt zufaellig eine der verfuegbaren Attacken des Angreifer-Pokemons 
+     * aus und fuehrt sie gegen das Ziel aus, sofern das Pokemon handlungsfaehig ist.
+     * * @param angreifer Das vom Computer gesteuerte Pokemon des Rivalen
+     * @param ziel      Das zu fokussierende Ziel-Pokemon des Spielers
+     * @param random    Der Zufallsgenerator fuer die Bestimmung des Attacken-Indexes
      */
     public static void fuehreZufallsAktionAus(Pokemon angreifer, Pokemon ziel, Random random) {
         if (angreifer.kannAgieren()) {
