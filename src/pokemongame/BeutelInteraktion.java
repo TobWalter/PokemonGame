9861f436c695a8770b9cb04f83ca6f3c48806bb1
Tobias Walter <tob.dev@outlook.com>
2026-05-23 01:07:02 +0200

    docs: korrigiere Javadoc-Formatierung in BeutelInteraktion.java
    - Bereinige fehlerhafte Syntax bei den @param-Tags der Methode oeffneBeutelMenue
    - Ergaenze die Parameter-Beschreibungen der UI-Steuerung

--- src/pokemongame/BeutelInteraktion.java
+++ src/pokemongame/BeutelInteraktion.java
@@ -3,18 +3,17 @@ package pokemongame;
 import java.util.Scanner;
 
 /**
- * Übernimmt ausschließlich die visuelle Darstellung und die Steuerung
- * des Beutel-Menüs auf der Konsole.
+ * Uebernimmt ausschließlich die visuelle Darstellung und die Steuerung
+ * des Beutel-Menues auf der Konsole.
  */
 public class BeutelInteraktion {
 
     /**
-     * oeffnet das Beutel-Menü auf der Konsole.
-     * 
-     * @param beutel Der Beutel, dessen Inhalt angezeigt werden soll
-     * @param meinPokemon Das aktive Pokémon, das das Item empfangen soll
-     * @param scanner Der Scanner für die Eingabe
-     * @return true, wenn ein Item erfolgreich verbraucht wurde, sonst false bei Abbruch.
+     * Oeffnet das Beutel-Menue auf der Konsole und verwaltet die Artikelauswahl.
+     * * @param beutel        Der Beutel, dessen Inhalt angezeigt werden soll
+     * @param meinPokemon   Das aktive Pokemon, das das Item empfangen soll
+     * @param scanner       Der Scanner für die Benutzereingabe
+     * @return true, wenn ein Item erfolgreich verbraucht wurde, sonst false bei Abbruch
      */
     public static boolean oeffneBeutelMenue(Beutel beutel, Pokemon meinPokemon, Scanner scanner) {
         System.out.println("\n--- DEIN BEUTEL ---");
@@ -41,14 +40,8 @@ public class BeutelInteraktion {
             return false; 
         }
 
-        // Item anwenden
-        int itemIndex = wahl - 1;
-        Item gewaehltesItem = beutel.getItem(itemIndex);
-
-        if (gewaehltesItem != null) {
-            return gewaehltesItem.benutzen(meinPokemon);
-        }
-
-        return false;
+        // Item heraussuchen und benutzen
+        Item gewaehltesItem = beutel.getItem(wahl - 1);
+        return gewaehltesItem.benutzen(meinPokemon);
     }
 }
